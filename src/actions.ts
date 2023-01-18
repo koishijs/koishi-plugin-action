b497644b7e9ae8c15f0416e6efe164ea4393cd21
TimeBather <open-source@timebather.cn>
2023-01-18 20:06:06 +0800

    Feat : add some actions(send-message,mute,recall)

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,5 @@
 import {Action} from "./registry";
+import {Schema} from "koishi";
 
 const LoggerAction : Action = {
   id: "logger",
@@ -7,6 +8,45 @@ const LoggerAction : Action = {
   }
 }
 
+interface SendMessageActionConfig{
+  message:string
+}
+const SendMessageAction : Action<SendMessageActionConfig> = {
+  id: "send-message",
+  Config:Schema.object({
+    message:Schema.string()
+  }),
+  async run(ctx,config,features){
+    if(features.session && features.session.channelId)
+      await features.session.send(config.message)
+  }
+}
+
+interface MuteActionConfig{
+  time:number
+}
+const MuteAction : Action<MuteActionConfig> = {
+  id: "mute",
+  Config:Schema.object({
+    time:Schema.number()
+  }),
+  async run(ctx,config,features){
+    if(features.session && features.session.guildId)
+      await features.session.bot.muteGuildMember(features.session.guildId,features.session.userId,config.time*1000)
+  }
+}
+
+const RecallMessageAction : Action = {
+  id: "recall-message",
+  async run(ctx,config,features){
+    if(features.session && features.session.messageId && features.session.guildId)
+      await features.session.bot.deleteMessage(features.session.cid,features.session.messageId)
+  }
+}
+
 export const PreRegisteredActions = [
-  LoggerAction
+  LoggerAction,
+  SendMessageAction,
+  MuteAction,
+  RecallMessageAction
 ]
