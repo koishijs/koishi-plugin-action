478ec2395b6c1ec0c7d39a1f47a26b19dbd42c89
idanran <idanran@outlook.com>
2023-07-13 20:33:35 +0800

    v0.2.0

---
+++ src/config.ts
@@ -0,0 +1,103 @@
+import { Schema } from "koishi"
+
+export const MessageContentMatcher = Schema.object({
+    type: Schema.const('message_content').required(),
+    regex: Schema.boolean().description("是否启用正则表达式功能").default(true).hidden(),
+    content: Schema.string().description("要匹配的内容 (支持正则表达式)").required()
+})
+
+export const SegmentTypeMatcher = Schema.object({
+    type: Schema.const('segment_type').required(),
+    segment_type: Schema.union([
+        Schema.const("xml").description("XML卡片消息"),
+        Schema.const("json").description("JSON卡片消息"),
+        Schema.const("quote").description("引用消息"),
+        Schema.const("at").description("艾特消息"),
+        Schema.const("file").description("文件消息"),
+        Schema.string().description("其他类型"),
+    ]).description("匹配的消息类型").default('xml')
+})
+
+export const Matcher = Schema.intersect([
+    Schema.object({
+        type: Schema.union([
+            Schema.const("message_content").description("匹配原始消息内容"),
+            Schema.const("segment_type").description("匹配消息类型")
+        ]).required().description('监听类型')
+    }),
+    Schema.union([MessageContentMatcher, SegmentTypeMatcher])
+] as const)
+
+export const RecallAction = Schema.object({
+    type: Schema.const('recall').required()
+})
+
+export const SendMessageAction = Schema.object({
+    type: Schema.const('send_message').required(),
+    content: Schema.string().description("发送的消息内容")
+})
+
+export const MuteAction = Schema.object({
+    type: Schema.const('mute').required(),
+    time: Schema.number().min(0).description("禁言时长 (单位: 未知)")
+})
+
+export const Action = Schema.intersect([
+    Schema.object({
+        type: Schema.union([
+            Schema.const("mute").description("禁言用户"),
+            Schema.const("recall").description("撤回消息"),
+            Schema.const("send_message").description("发送消息")
+        ]).required().description("操作类型")
+    }),
+    Schema.union([RecallAction, SendMessageAction, MuteAction])
+] as const)
+
+export const Config: Schema<Config> = Schema.object({
+    actions: Schema.array(Schema.object({
+        name: Schema.string().description("规则名称"),
+        matchers: Schema.array(Matcher).description("匹配器, 只有所有匹配器都满足才会执行Actions中的内容").default([]),
+        actions: Schema.array(Action).description("执行器, 做一些事情").default([])
+    })).default([])
+})
+
+export interface ActionEntries {
+    name: string,
+    matchers: Matcher[]
+    actions: Action[]
+}
+
+export interface Config {
+    actions: ActionEntries[]
+}
+
+
+export interface MuteAction {
+    type: 'mute',
+    time: number
+}
+
+export interface SendMessageAction {
+    type: 'send_message',
+    content: string
+}
+
+export interface RecallAction {
+    type: 'recall'
+}
+
+export type Action = RecallAction | SendMessageAction | MuteAction
+
+
+export interface SegmentTypeMatcher {
+    type: 'segment_type',
+    segment_type: string
+}
+
+export interface MessageContentMatcher {
+    type: 'message_content',
+    regex: boolean,
+    content: string
+}
+
+export type Matcher = MessageContentMatcher | SegmentTypeMatcher;
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,55 @@
-import { Context, Schema } from 'koishi'
-import {Action, actions} from "./actions";
-import {Matcher, matchers} from "./matchers";
+import { Context, Session } from 'koishi'
+import { Config, MessageContentMatcher, SegmentTypeMatcher, RecallAction, SendMessageAction, MuteAction } from './config'
+
 export const name = 'action'
+export { Config }
 
-export interface ActionEntries{
-  name:string,
-  matchers:Matcher[]
-  actions:Action[]
+function messageContentMatcher(session: Session, config: MessageContentMatcher) {
+  if (config.regex)
+    return new RegExp(config.content).test(session.content)
+  return session.content.includes(config.content)
 }
 
-export interface Config {
-  actions:ActionEntries[]
+function segmentTypeMatcher(session: Session, config: SegmentTypeMatcher) {
+  if (!session.elements) return false
+  return session.elements.some(t => t.type === config.segment_type)
 }
 
-export const Config: Schema<Config> = Schema.object({
-  actions:Schema.array(Schema.object({
-    name:Schema.string().description("规则名称"),
-    matchers:Schema.array(Matcher).description("匹配器,只有所有匹配器都满足才会执行Actions中的内容"),
-    actions:Schema.array(Action).description("执行器,做一些事情")
-  }))
-}) as any;
+const matchers = {
+  message_content: messageContentMatcher,
+  segment_type: segmentTypeMatcher
+}
 
-export function apply(ctx: Context,config:Config) {
-  const logger = ctx.logger("action")
-  ctx.middleware((session, next)=>{
-    if(config.actions){
-      config.actions.forEach(action=>{
-        if(!action.matchers || !action.matchers.length){
-          logger.warn("The action ["+(action.name)+"] has no matcher. It will be ignored.")
-        }
-        if(action.matchers.every(matcher=>matchers[matcher.type](session,matcher as any))){
-          logger.info("Running action",action.name)
-          action.actions.forEach(action=>actions[action.type](session,action as any))
-        }
-      })
+async function recallAction(session: Session, config: RecallAction) {
+  await session.bot.deleteMessage(session.channelId, session.messageId)
+}
+
+async function sendMessageAction(session: Session, config: SendMessageAction) {
+  await session.send(config.content)
+}
+
+async function muteAction(session: Session, config: MuteAction) {
+  await session.bot.muteGuildMember(session.guildId, session.userId, config.time)
+}
+
+const actions = {
+  recall: recallAction,
+  send_message: sendMessageAction,
+  mute: muteAction
+}
+
+export function apply(ctx: Context, config: Config) {
+  const logger = ctx.logger('action')
+  ctx.middleware((session, next) => {
+    for (const action of config.actions) {
+      if (action.matchers.length === 0) {
+        logger.warn(`The action [${action.name}] has no matcher. It will be ignored.`)
+      }
+      if (action.matchers.every(matcher => matchers[matcher.type](session, matcher as any))) {
+        logger.info('Running action', action.name)
+        action.actions.forEach(action => actions[action.type](session, action as any))
+      }
     }
-    return next();
+    return next()
   })
 }
